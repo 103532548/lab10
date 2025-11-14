66d94ed1ff6ef5e001ff0499cc801d922498eb0f
minhnt minhnt@xdp.vn <minhnt@ozc-company.com>
2025-11-14 10:57:05 +0700

    c

--- profile.php
+++ profile.php
@@ -2,21 +2,54 @@
 
 session_start();
 
-if(!isset($_POST['username']))
+$host = "localhost";         // because XAMPP runs the server locally
+$user = "root";          // default username for XAMPP's MySQL
+$pwd = "";              // default password is empty in XAMPP
+$sql_db = "login";  // replace with the actual name of your database
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+if (!$conn) 
 {
-    header("Location: login.php");
-    exit();
+    die("Connection failed: " . mysqli_connect_error());
 }
 else
 {
-    echo 
-    "
-    <h2>Profile</h2>
-    <tr>
-    <td>{$row['username']}</td>
-    <td>{$row['email']}</td>
-    </tr>
-    ";
+    $result = mysqli_query($conn, "SELECT * FROM users WHERE username='$username' AND password='$password'");
+    
+
+    if(!$result)
+    {
+        header("Location: login.php");
+        exit();
+    }
+    else
+    {
+        echo "<h2>Profile</h2>";
+        echo 
+        "
+        <table border='1'>
+        <tr>
+        <th>Username</th>
+        <th>Email</th>
+        </tr>
+        ";
+
+        while($row = mysqli_fetch_assoc($result))
+        {
+            echo 
+            "
+            <tr>
+            <td>{$row['username']}</td>
+            <td>{$row['email']}</td>
+            </tr>
+            ";
+        }
+
+        echo "</table>";
+        mysqli_free_result($result);
+
+    }
 }
 
 ?>
