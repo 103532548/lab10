67cc5f4c6c18ab7a2e0f4917e9f34cbe81234711
minhnt minhnt@xdp.vn <minhnt@ozc-company.com>
2025-11-14 11:19:05 +0700

    new

--- update_profile.php
+++ update_profile.php
@@ -8,3 +8,22 @@
 </form>
 </body>
 </html>
+
+<?php
+
+session_start();
+
+$host = "localhost";         // because XAMPP runs the server locally
+$user = "root";          // default username for XAMPP's MySQL
+$pwd = "";              // default password is empty in XAMPP
+$sql_db = "login";  // replace with the actual name of your database
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+$username=$_SESSION['username'];
+$email=$_POST['email'];
+
+mysqli_query($conn,"UPDATE user SET email='$email' WHERE username='$username'");
+header("Location: profile.php");
+
+?>
